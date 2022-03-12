89e7a27e0750b418eb1e6df5ee705a91c8b5ae7e
Jon Dough <49297268+jetstream0@users.noreply.github.com>
2022-03-12 23:40:13 +0000

    finished config.js

--- config.js
+++ config.js
@@ -54,6 +54,9 @@ for (let c_num = 0; c_num < coins.length; c_num++) {
     if (config[coin].rpc == undefined) {
       config[coin].rpc = default_rpcs[coin];
     }
+    if (config[coin].default == undefined) {
+      config[coin].default = false;
+    }
     //if `percentage` is false, will be random in between max and min. if `percentage` is not false, `percentage` will be the percent of the faucet bal it should be, still adhering to max and min
     if (config[coin].payouts.percentage == undefined) {
       config[coin].payouts.percentage = false;
@@ -61,11 +64,11 @@ for (let c_num = 0; c_num < coins.length; c_num++) {
   }
 }
 
-//create a package.json file based on settings, including command to run index.js (with parameters ofc)
+/* create a package.json file based on settings, including command to run index.js (with parameters ofc) */
 
 //description, dependencies, keywords
 //add node fetch to deps? or maybe axios
-let dependencies = {'nunjucks': '^3.2.3', 'express': '^4.17.1', 'body-parser': '^1.19.0', 'cookie-parser': '^1.4.5', 'mongodb': '^3.6.6'};
+let dependencies = {'nunjucks': '^3.2.3', 'express': '^4.17.1', 'body-parser': '^1.19.0', 'cookie-parser': '^1.4.5', 'mongodb': '^3.6.6', 'node-fetch': '^3.2.3'};
 let coin_deps = {
   'xdai': ['ethers', '^5.5.1'],
   'banano': ['bananojs', 'npm:@bananocoin/bananojs@^2.4.24'],
@@ -73,31 +76,61 @@ let coin_deps = {
 };
 
 //add dependencies
+let activated_coin_num = 0;
 for (let c_num2 = 0; c_num2 < coins.length; c_num2++) {
   let coin = coins[c_num2];
   if (config[coin].enabled) {
+    activated_coin_num++;
     dependencies[coin_deps[coin][0]] = coin_deps[coin][1];
   }
 }
 
-/*
-{
+if (activated_coin_num == 0) {
+  throw new Error('Faucet has no coins');
+}
+
+//generate description, keywords
+let keywords = ['"crypto"','"faucet"'];
+let description = 'A ';
+let activated_coins = [];
+for (let c_num2 = 0; c_num2 < coins.length; c_num2++) {
+  let coin = coins[c_num2];
+  if (config[coin].enabled) {
+    keywords.push('"'+coin+'"');
+    if (activated_coin_num == coins.length) {
+      description += coin+' ';
+    } else if (activated_coin_num != 1) {
+      description += coin+', ';
+    } else {
+      description += coin+' ';
+    }
+  }
+}
+description += 'cryptocurrency faucet made by Prussia. And run by ';
+description += config.owner;
+
+//change dependencies into readable form
+let dependencies_string = "";
+for (let dep_num=0; dep_num < Object.keys(dependencies).length; dep_num++) {
+  dependencies_string += "\n    "Object.keys(dependencies)[dep_num]+": "+'"'+[dependenciesObject].keys(dependencies)[dep_num]]+'",';
+}
+
+let package_js_npm_str = `{
   "name": "Prussia-Faucet",
   "version": "2.0.0",
-  "description": {{ description }},
+  "description": `+description+`,
   "main": "index.js",
   "scripts": {
     "test": "echo \"Error: no test specified\" && exit 1"
   },
-  "keywords": [{{ keywords }}],
+  "keywords": [`+keywords.join(', ')+`],
   "author": "Jetstream0 <@prussia.dev>",
   "license": "MIT",
-  "dependencies": {
-    {% for dependency in dependencies %}
-      {{ dependency }},
-    {% endfor %}
+  "dependencies": {`+dependencies_string+`
   }
-}
-*/
+}`;
+
+//write to package.json
+fs.writeFileSync('package.json', package_js_npm_str);
 
 module.exports = config;
