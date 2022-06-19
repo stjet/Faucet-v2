105f709f08f4cfd4c42443218fb9c3b1d07c0eb4
Ash Bauer <105770584+HalfBakedBread@users.noreply.github.com>
2022-06-19 18:57:04 -0500

    added missing util exports

--- util.js
+++ util.js
@@ -179,6 +179,9 @@ function calculate_payouts(config_payouts) {
 }
 
 module.exports = {
+  get_number_decimal_zeros: get_number_decimal_zeros,
+  truncate_to_decimals: truncate_to_decimals,
+  format_amount_decimals: format_amount_decimals,
   milliseconds_to_readable: milliseconds_to_readable,
   insert: insert,
   replace: replace,
